8cf108fc4a821bf895e1ff3d93fbe0f6e341aa76
= <12beattiecastp@gmail.com>
2025-08-14 16:18:44 +0100

    fix: better file storage

--- backend/src/lib/fileStorage.js
+++ backend/src/lib/fileStorage.js
@@ -3,37 +3,69 @@ const { mkdirSync, existsSync } = require('fs');
 const { writeFile, readFile } = require('fs/promises');
 const { v4: uuidv4 } = require('uuid');
 
-// TODO: Can't use public dir, it's unrestricted, save private images, and send them to client when needed
-// If you look at this, ignore it for now, images that should be protected should not be handled like this
 class FileStorage {
-  static path = join(process.cwd(), 'public', 'files');
+  static path = join(process.cwd(), 'storage', 'files');
   static imagesPath = join(this.path, 'images');
 
+  static acceptedImageFileTypes = ['image/jpeg', 'image/png', 'image/jpg'];
+  static maxImageSizeMb = 10;
+  static mimeTypeToExtensionMap = {
+    'image/jpeg': 'jpeg',
+    'image/png': 'png',
+    'image/jpg': 'jpg'
+  }
+
   static initialize() {
-    new this().createDirIfNotExists(this.path);
+    this.createDirIfNotExists(this.path);
   }
 
-  createDirIfNotExists(path) {
+  static createDirIfNotExists(path) {
     if (!existsSync(path)) {
-      mkdirSync(path);
+      mkdirSync(path, { recursive: true });
     }
   }
 
-  async saveImage(contents, ext) {
-    this.createDirIfNotExists(FileStorage.imagesPath);
+  static bytesToMb(bytes) {
+    if (bytes > 0) {
+      return bytes / 1e+6;
+    }
 
-    const fileSaveName = uuidv4();
-    const path = join(FileStorage.imagesPath, fileSaveName + ext);
-
-    await writeFile(path, contents);
-
-    return path;
+    return 0;
   }
 
-  async getImage(path) {
-    if (existsSync(path)) {
-      return readFile(path, 'base64');
+  static async validImageFile(file) {
+    if (file.size > this.maxImageSizeMb) {
+      return false;
     }
+
+    if (!this.acceptedImageFileTypes.includes(file.mimetype)) {
+      return false;
+    }
+
+    return true;
+  }
+
+  static getFileExtensionFromMetadata(mimetype) {
+    return '.' + this.mimeTypeToExtensionMap[mimetype] || '';
+  }
+
+  static async saveImage(file) {
+    if (!this.validImageFile(file)) {
+      throw new Error('Invalid image file');
+    }
+
+    this.createDirIfNotExists(this.imagesPath);
+
+    const fileSaveName = uuidv4() + this.getFileExtensionFromMetadata(file.mimetype);
+    const path = join(this.imagesPath, fileSaveName);
+
+    await writeFile(path, file.buffer);
+
+    return fileSaveName;
+  }
+
+  static getImagePath(imageName) {
+    return join(this.imagesPath, imageName);
   }
 }
 
