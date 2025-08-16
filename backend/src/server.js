006c436448b33309c7fa09f84c524627dc0c6ce1
carlbeattie2000 <12beattiecastp@gmail.com>
2025-08-16 08:33:30 +0100

    add: cookie parser

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,16 @@
-require('dotenv').config();
-const express = require('express');
-const router = require('./routes');
-const registerSwaggerForDevEnv = require('./config/swagger');
-const usePublicDir = require('./config/staticFiles');
+require("dotenv").config();
+const express = require("express");
+const router = require("./routes");
+const cookieParser = require("cookie-parser");
+
+const registerSwaggerForDevEnv = require("./config/swagger");
+const usePublicDir = require("./config/staticFiles");
 
 const app = express();
 
 registerSwaggerForDevEnv(app);
 
+app.use(cookieParser());
 app.use(express.static(usePublicDir()));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
