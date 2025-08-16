1e31662fbb0fdb06e8384137c10d1ca672e53470
carlbeattie2000 <12beattiecastp@gmail.com>
2025-08-16 10:12:18 +0100

    feat: report status

--- backend/src/models/report.model.js
+++ backend/src/models/report.model.js
@@ -2,21 +2,22 @@ const BaseModel = require("./base.model");
 
 class ReportModel extends BaseModel {
   static table = "reports";
-  static schema = `CREATE TABLE IF NOT EXISTS ${this.table} (id INTEGER PRIMARY KEY AUTOINCREMENT, description TEXT, longitude REAL, latitude REAL, user_id INTEGER, createdAt DATE DEFAULT current_date)`;
-
+  static schema = `CREATE TABLE IF NOT EXISTS ${this.table} (id INTEGER PRIMARY KEY AUTOINCREMENT, description TEXT, longitude REAL, latitude REAL, user_id INTEGER, status TEXT DEFAULT "PENDING", createdAt DATE DEFAULT current_date)`;
   description;
   longitude;
   latitude;
   user_id;
+  status;
   createdAt = null;
 
-  constructor(description, longitude, latitude, user_id) {
+  constructor(description, longitude, latitude, user_id, status) {
     super();
 
     this.description = description;
     this.longitude = longitude;
     this.latitude = latitude;
     this.user_id = user_id;
+    this.status = status;
   }
 }
 
