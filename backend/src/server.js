3943086c4bbd75d6835c4295c3691b0815b9ca93
carlbeattie2000 <12beattiecastp@gmail.com>
2025-08-22 00:47:00 +0100

    remove: template engine and public dir

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,9 +4,7 @@ const router = require("./routes");
 const cookieParser = require("cookie-parser");
 
 const registerSwaggerForDevEnv = require("./config/swagger");
-const usePublicDir = require("./config/static-files");
 const HttpErrorMiddleware = require("./middleware/errors.middleware");
-const useTemplateEngine = require("./config/template-engine");
 const securityHeadersMiddleware = require("./middleware/security-headers.middleware");
 const notFoundMiddleware = require("./middleware/not-found.middleware");
 const rateLimitMiddleware = require("./middleware/rate-limiting.middleware");
@@ -16,16 +14,12 @@ const app = express();
 registerSwaggerForDevEnv(app);
 
 app.use(cookieParser());
-app.use(express.static(usePublicDir()));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(securityHeadersMiddleware);
 app.disable("x-powered-by");
 app.use(router);
 app.use(rateLimitMiddleware());
-
-// Only enabled when dev env set to DEVELOPMENT
-useTemplateEngine(app);
 
 app.use(notFoundMiddleware);
 app.use(HttpErrorMiddleware);
