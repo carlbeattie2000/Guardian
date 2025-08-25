247d7e7cf52330376b26fa379a1be372f39b8246
carlbeattie2000 <12beattiecastp@gmail.com>
2025-08-25 18:07:51 +0100

    feat(mapBox): give authenticated users mapbox tokens

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -5,6 +5,7 @@ const rateLimitMiddleware = require("../middleware/rate-limiting.middleware");
 const reportsRouter = require("./reports.route");
 const lostArticlesRouter = require("./lost-articles.route");
 const authenticationRouter = require("./authentication.route");
+const mapBoxRouter = require("./map-box.route");
 const fileRouter = require("./files.route");
 
 const router = Router();
