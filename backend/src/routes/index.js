3f3bc0407036cf70471f7aa9e5c12b268d7007b3
carlbeattie2000 <12beattiecastp@gmail.com>
2025-08-25 18:07:09 +0100

    feat(files): authentication for images independent of main authentication

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -5,6 +5,7 @@ const rateLimitMiddleware = require("../middleware/rate-limiting.middleware");
 const reportsRouter = require("./reports.route");
 const lostArticlesRouter = require("./lost-articles.route");
 const authenticationRouter = require("./authentication.route");
+const fileRouter = require("./files.route");
 
 const router = Router();
 
@@ -19,5 +20,7 @@ router.use(
   AuthorisationMiddleware,
   lostArticlesRouter,
 );
+router.use("/api/v1/map-box", AuthorisationMiddleware, mapBoxRouter);
+router.use("/api/v1/files", fileRouter);
 
 module.exports = router;
