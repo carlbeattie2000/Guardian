98702b31e8e9102e88d438fd80f9e1b9328bd7aa
carlbeattie2000 <12beattiecastp@gmail.com>
2025-08-27 22:30:09 +0100

    feat(ExpressRequestMock): model closer to original

--- backend/test/testing-utils/expressRequest.js
+++ backend/test/testing-utils/expressRequest.js
@@ -1,6 +1,8 @@
 class ExpressMockRequest {
   static new() {
-    return {
+    const request = {
+      finshed: false,
+      destroyed: false,
       headers: {},
       body: undefined,
       cookies: {},
@@ -8,7 +10,73 @@ class ExpressMockRequest {
       params: {},
       query: {},
       res: null,
+      maxHeadersCount: 2000,
+      path: "",
+      method: "",
+      host: "",
+      protocol: "",
+
+      getHeader(name) {
+        this.headers[name];
+        return this;
+      },
+      setHeader(name, value) {
+        this.headers[name] = value;
+        return this;
+      },
+      getHeaderNames() {
+        return Object.keys(this.headers);
+      },
+      getHeaders() {
+        return Object.fromEntries(
+          Object.entries(this.headers).map(([key, value]) => [
+            key.toLowerCase(),
+            value,
+          ]),
+        );
+      },
+      getRawHeaderNames() {
+        return Object.keys(this.headers);
+      },
+      hasHeader(name) {
+        return Object.prototype.hasOwnProperty.call(this.headers, name);
+      },
+      removeHeader(name) {
+        if (this.hasHeader(name)) {
+          delete this.headers[name];
+        }
+      },
+      end(chunk, encoding, callback) {
+        this.finshed = true;
+      },
+      destroy(error) {
+        if (this.destroyed) {
+          return this;
+        }
+
+        this.destroyed = true;
+
+        return this;
+      },
     };
+
+    Object.defineProperty(request, "writableFinished", {
+      get() {
+        return this._finshed === true;
+      },
+      enumerable: true,
+      configurable: true,
+    });
+
+    Object.defineProperty(request, "writableEnded", {
+      get() {
+        return this._finshed === true;
+      },
+      enumerable: true,
+      configurable: true,
+    });
+
+    return request;
   }
 }
 
