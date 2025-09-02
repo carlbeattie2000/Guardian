f92a6de1731014715ffee8f3af17ae4a9f4dcba1
carlbeattie2000 <12beattiecastp@gmail.com>
2025-09-02 11:09:47 +0100

    feat: better UI, other tweaks

--- localModel/client.js
+++ localModel/client.js
@@ -12,8 +12,7 @@ async function loadModel() {
 	return regression;
 }
 
-async function predict(regression, text) {
-	const model = await use.load();
+async function predict(model, regression, text) {
 	const embedding = await model.embed([text]);
 	const embeddingArray = await embedding.array();
 	embedding.dispose();
@@ -21,54 +20,63 @@ async function predict(regression, text) {
 	const prediction = regression.predict(embeddingArray);
 	const priorityScore = prediction[0][0];
 
-	return `Priority score: ${priorityScore < 0 ? 0 : priorityScore * 100}`;
+	return `Priority score: ${priorityScore < 0 ? 0 : (Math.round(priorityScore * 100) / 100) * 100}`;
 }
 
-http
-	.createServer(async (req, res) => {
-		console.log("Loading Model");
-		const regression = await loadModel();
-		console.log("Model Loaded");
+(async () => {
+	console.log("Loading Model");
+	const regression = await loadModel();
+	const model = await use.load();
+	console.log("Model Loaded");
 
-		let body = "";
+	http
+		.createServer(async (req, res) => {
+			let body = "";
 
-		req.on("readable", () => {
-			const chunk = req.read();
-			if (chunk !== null) {
-				body += chunk;
+			req.on("readable", () => {
+				const chunk = req.read();
+				if (chunk !== null) {
+					body += chunk;
+				}
+			});
+
+			if (req.url === "/") {
+				switch (req.method) {
+					case "GET":
+						{
+							res.setHeader("Content-Type", "text/html");
+							const document = await readFile("./client.html", "utf8");
+							res.end(document);
+						}
+						return;
+					case "POST":
+						{
+							req.on("end", async () => {
+								if (req.headers["content-type"] === "application/json") {
+									body = JSON.parse(body);
+								}
+								const prediction = await predict(
+									model,
+									regression,
+									body.message,
+								);
+
+								res.setHeader("Content-Type", "text/plain");
+								res.statusCode = 200;
+								res.write(prediction);
+								res.end();
+							});
+						}
+						return;
+					default:
+						break;
+				}
 			}
+
+			res.statusCode = 404;
+			res.end();
+		})
+		.listen(3000, () => {
+			console.log("Server listening @ http://localhost:3000");
 		});
-
-		if (req.url === "/") {
-			switch (req.method) {
-				case "GET":
-					{
-						res.setHeader("Content-Type", "text/html");
-						const document = await readFile("./client.html", "utf8");
-						res.end(document);
-					}
-					return;
-				case "POST":
-					{
-						req.on("end", async () => {
-							if (req.headers["content-type"] === "application/json") {
-								body = JSON.parse(body);
-							}
-							const prediction = await predict(regression, body.message);
-
-							res.setHeader("Content-Type", "text/plain");
-							res.statusCode = 200;
-							res.write(prediction);
-							res.end();
-						});
-					}
-					return;
-				default:
-					break;
-			}
-		}
-
-		res.statusCode = 404;
-		res.end();
-	})
-	.listen(3000);
+})();
