59997395664c97d7261b7ebd6a17e7d1e46ab783
carlbeattie2000 <12beattiecastp@gmail.com>
2025-09-04 13:48:55 +0100

    chore: format

--- client/services/apiService.ts
+++ client/services/apiService.ts
@@ -5,27 +5,28 @@ const BASE_URL = "http://192.168.0.238:2699";
 const USE_COOKIES = true;
 
 const apiService = axios.create({
-  baseURL: BASE_URL,
+	baseURL: BASE_URL,
 });
 
 apiService.interceptors.request.use(async function (config) {
-  try {
-    const accessToken = await SecureStore.getItemAsync("accessToken");
-    const refreshToken = await SecureStore.getItemAsync("refreshToken");
+	try {
+		const accessToken = await SecureStore.getItemAsync("accessToken");
+		const refreshToken = await SecureStore.getItemAsync("refreshToken");
 
-    if (accessToken) {
-      config.headers.set("Authorization", `Bearer ${accessToken}`);
-    }
-    if (refreshToken) {
-      config.headers.set("Refresh-Token", refreshToken);
-    }
-  } catch (err) {
-    console.error(err);
-  }
+		if (accessToken) {
+			config.headers.set("Authorization", `Bearer ${accessToken}`);
+		}
+		if (refreshToken) {
+			config.headers.set("Refresh-Token", refreshToken);
+		}
+	} catch (err) {
+		console.error(err);
+	}
 
-  config.withCredentials = USE_COOKIES;
-  config.validateStatus = (status) => status < 500;
-  return config;
+	config.withCredentials = USE_COOKIES;
+	config.validateStatus = (status) => status < 500;
+	return config;
 });
 
 export { apiService, BASE_URL };
+
