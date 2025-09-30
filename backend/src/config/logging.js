db426f8af7fd0e7ae05cd47c0cacf8c372b11ae5
Carl Beattie <contact.carlbeattie@gmail.com>
2025-09-30 12:17:26 +0100

    fix(winston): place logs inside logs folder

--- backend/src/config/logging.js
+++ backend/src/config/logging.js
@@ -1,18 +1,19 @@
 const winston = require("winston");
+const { join } = require("node:path");
 
 const defaultLogger = winston.createLogger({
-  level: "info",
+  level: 'info',
   format: winston.format.json(),
   transports: [
     new winston.transports.File({
-      filename: "http.log",
+      filename: join("logs", "http.log"),
       level: "http",
     }),
     new winston.transports.File({
-      filename: "error.log",
+      filename: join("logs", "error.log"),
       level: "error",
     }),
-    new winston.transports.File({ filename: "combined.log" }),
+    new winston.transports.File({ filename: join("logs", "combined.log") }),
   ],
 });
 
